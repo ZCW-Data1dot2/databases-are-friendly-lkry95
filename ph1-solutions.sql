c7d7199bbff511f3429e300e66b2a68bedbe1daf
Luke Roy <lkry95@gmail.com>
2020-12-05 16:29:20 -0500

    Update ph1-solutions.sql

--- ph1-solutions.sql
+++ ph1-solutions.sql
@@ -4,3 +4,35 @@
 USE sakila;
 
 -- Your solutions...
+i. select first_name, last_name
+from actor
+where first_name = "Scarlett";
+
+ii. select first_name, last_name
+from actor
+where last_name = "Johansson";
+
+iii. select count(distinct last_name) 
+from actor;
+
+iv. select last_name 
+from actor 
+group by last_name 
+where count(*) < 2;
+
+v. select last_name 
+from actor 
+group by last_name 
+where count(*) > 1;
+
+vi. select actor.actor_id, actor.first_name, actor.last_name, count(actor_id) as num_films
+from actor 
+join film_actor using (actor_id)
+group by actor_id
+order by num_films desc
+limit 1;
+
+vii. select film.film_id, film.title, store.store_id, inventory.inventory_id
+from inventory join store using (store_id) 
+join film using (film_id)
+where film.title = 'Academy Dinosaur' and store.store_id = 1;
