703eb48be9400dff73c874eeed8ad91972e8a3be
Luke Roy <lkry95@gmail.com>
2020-12-06 10:34:25 -0500

    Update ph1-solutions.sql

--- ph1-solutions.sql
+++ ph1-solutions.sql
@@ -36,3 +36,38 @@ vii. select film.film_id, film.title, store.store_id, inventory.inventory_id
 from inventory join store using (store_id) 
 join film using (film_id)
 where film.title = 'Academy Dinosaur' and store.store_id = 1;
+
+select inventory.inventory_id
+from inventory join store using (store_id)
+     join film using (film_id)
+     join rental using (inventory_id)
+where film.title = 'Academy Dinosaur'
+      and store.store_id = 1
+      and not exists (select * from rental
+                      where rental.inventory_id = inventory.inventory_id
+                      and rental.return_date is null);
+
+viii. insert into rental (rental_date, inventory_id, customer_id, staff_id)
+values (NOW(), 1, 1, 1);
+                      
+ix. select rental_duration 
+from film 
+where film_id = 1;
+
+select rental_id from rental order by rental_id desc limit 1;
+
+select rental_date,
+       rental_date + interval
+                   (select rental_duration from film where film_id = 1) day
+                   as due_date
+from rental
+where rental_id = (select rental_id from rental order by rental_id desc limit 1);
+
+x. select avg(length) from film;
+
+xi. select category.name, avg(length)
+from film join film_category using (film_id) join category using (category_id)
+group by category.name
+order by avg(length) desc;
+
+xii. No data.
